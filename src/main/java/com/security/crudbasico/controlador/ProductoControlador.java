d8a99af58e3975ded51cb69d749d136466796a9f
EmiroMunoz01 <emiro_2001@icloud.com>
2025-04-23 09:53:32 -0500

    Crud finalizado, se tienen las operaciones basicas del mismo y funcionan de forma correcta

--- src/main/java/com/security/crudbasico/controlador/ProductoControlador.java
+++ src/main/java/com/security/crudbasico/controlador/ProductoControlador.java
@@ -3,10 +3,12 @@ package com.security.crudbasico.controlador;
 
 import com.security.crudbasico.Servicio.ProductoServicio;
 import com.security.crudbasico.modelo.Producto;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/productos")
@@ -25,12 +27,61 @@ public class ProductoControlador {
     }
 
     @PostMapping("/crear")
-    public ResponseEntity<?> crearProducto(@RequestBody Producto producto){
+    public ResponseEntity<?> crearProducto(@Valid @RequestBody Producto producto) {
 
         Producto productoNuevo = servicio.guardar(producto);
 
         return ResponseEntity.ok(productoNuevo);
 
     }
+
+    @GetMapping("/buscar/{id}")
+    public ResponseEntity<?> obtenerProductoPorId(@PathVariable Long id) {
+
+        Optional<Producto> productoOpt = servicio.obtenerPorId(id);
+
+        if (productoOpt.isPresent()) {
+            Producto producto = productoOpt.get();
+            return ResponseEntity.ok(producto);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+
+    }
+
+    @PutMapping("/actualizar/{id}")
+    public ResponseEntity<Producto> actualizar(@RequestBody Producto producto, @PathVariable Long id) {
+
+        Optional<Producto> productoOptional = servicio.obtenerPorId(id);
+
+        if (productoOptional.isPresent()) {
+            Producto existente = productoOptional.get();
+            existente.setNombre(producto.getNombre());
+            existente.setPrecio(producto.getPrecio());
+            Producto actualizado = servicio.guardar(existente);
+            return ResponseEntity.ok(actualizado);
+
+        } else {
+
+            return ResponseEntity.notFound().build();
+
+        }
+
+    }
+
+    @DeleteMapping("/eliminar/{id}")
+    public ResponseEntity<Void> eliminar(@PathVariable Long id) {
+        Optional<Producto> productoOptional = servicio.obtenerPorId(id);
+
+        if (productoOptional.isPresent()) {
+            servicio.eliminar(id);
+            return ResponseEntity.noContent().build(); // 204 sin cuerpo
+        } else {
+            return ResponseEntity.notFound().build(); // 404 si no existe
+        }
+    }
+
+
+
 
 }
--- src/main/java/com/security/crudbasico/modelo/Producto.java
+++ src/main/java/com/security/crudbasico/modelo/Producto.java
@@ -5,6 +5,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -22,7 +24,10 @@ public class Producto {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @NotBlank(message = "El nombre es obligatorio")
     private String nombre;
 
+
+    @NotNull(message = "El precio es obligatorio")
     private Double precio;
 }
